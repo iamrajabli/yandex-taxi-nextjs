03ae3c929fd5ab531ac2fc3a148656a3dc280cf8
Hikmat Rajabli <iamrajabli@outlook.com>
2022-08-31 23:27:38 +0400

    Фокус на поле (useRef)

--- app/components/ui/InputPlaces.tsx
+++ app/components/ui/InputPlaces.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
 import { FiSearch } from 'react-icons/fi';
 import cn from 'classnames';
@@ -12,6 +12,13 @@ const InputPlaces: React.FC<IInputPlaces> = ({ cbSuccess, type }) => {
 
     const [address, setAddress] = useState('');
     const isFrom = type === 'from';
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const setFocus = () => inputRef?.current?.focus()
+
+    useEffect(() => {
+        if (isFrom) setFocus()
+    }, [isFrom])
 
     return (
         <PlacesAutocomplete
@@ -27,15 +34,20 @@ const InputPlaces: React.FC<IInputPlaces> = ({ cbSuccess, type }) => {
                     })}>
                     <div
                         className='py-4 px-5 bg-white rounded-lg flex items-center'
-                        style={suggestions.length ? 
+                        style={suggestions.length ?
                             {
                                 borderBottomLeftRadius: 0,
                                 borderBottomRightRadius: 0
                             } : {}}>
-                        <FiSearch color={isFrom ? '#ffbc00' : '#615f5d'} className='mr-3' size={26} />
+                        <FiSearch
+                            onClick={setFocus}
+                            color={isFrom ? '#ffbc00' : '#615f5d'}
+                            className='mr-3'
+                            size={26} />
                         <input {...getInputProps({
                             placeholder: isFrom ? 'Where from?' : 'Where to',
-                            className: 'outline-none w-full text-gray-800'
+                            className: 'outline-none w-full text-gray-800',
+                            ref: inputRef
                         })} />
 
                         {!isFrom && (
